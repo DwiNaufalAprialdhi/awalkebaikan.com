d5c95be0c1a46292e0d55ee082cf8f531fa2f373
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-07 10:31:51 +0700

    add page donation and page payment

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
+
+
 // visible Toggle 
 const visible = document.querySelector('.visible')
 const unvisible = document.querySelector('.unvisible')
@@ -25,3 +27,53 @@ function unvisibleToggle(){
         unvisible.classList.toggle('hidden')
     }
 }
+
+// copy Nominal
+function copyText() {
+  var nominal = document.getElementById("nominalInput");
+
+  nominal.select();
+  nominal.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(nominal.value);  
+}
+
+// copy Rekening
+function copyTextCredit() {
+  var credit = document.getElementById("creditInput");
+
+  credit.select();
+  credit.setSelectionRange(0, 99999);
+
+  navigator.clipboard.writeText(credit.value);  
+}
+
+// countdown Payment
+// Set waktu yang ditentukan
+var countDownDate = new Date("Dec 3, 2023 23:59:00").getTime();
+
+// update setiap 1 detik
+var x = setInterval(function() {
+
+  // Get hari dan waktu
+  var now = new Date().getTime();
+    
+  // Get hari dan waktu untuk bersamaan
+  var distance = countDownDate - now;
+    
+  // Time kalkulasi
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Kirim berdasarkan id
+  document.getElementById("countdown").innerHTML = days + "d:" + hours + ":"
+  + minutes + ":" + seconds + "";
+    
+  // Jika waktu sudah tiba maka kadaluarsa
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
