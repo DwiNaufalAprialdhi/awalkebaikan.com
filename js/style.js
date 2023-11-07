a3ca337b66c71a3b9b02fd9118a928a2a92c32b4
dwinaufalaprialdhi <dwinaufalaprialdhi@gmail.com>
2023-11-07 11:51:42 +0700

    add page account

--- js/style.js
+++ js/style.js
@@ -1,4 +1,33 @@
+// countdown Payment
 
+// Set waktu yang ditentukan
+var countDownDate = new Date("Dec 3, 2023 23:59:00").getTime();
+
+// update setiap 1 detik
+var x = setInterval(function() {
+
+  // Get hari dan waktu
+  var now = new Date().getTime();
+    
+  // Get hari dan waktu untuk bersamaan
+  var distance = countDownDate - now;
+    
+  // Time kalkulasi
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Kirim berdasarkan id
+  document.getElementById("countdown").innerHTML = days + "d:" + hours + ":"
+  + minutes + ":" + seconds + "";
+    
+  // Jika waktu sudah tiba maka kadaluarsa
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 // visible Toggle 
 const visible = document.querySelector('.visible')
@@ -47,33 +76,3 @@ function copyTextCredit() {
 
   navigator.clipboard.writeText(credit.value);  
 }
-
-// countdown Payment
-// Set waktu yang ditentukan
-var countDownDate = new Date("Dec 3, 2023 23:59:00").getTime();
-
-// update setiap 1 detik
-var x = setInterval(function() {
-
-  // Get hari dan waktu
-  var now = new Date().getTime();
-    
-  // Get hari dan waktu untuk bersamaan
-  var distance = countDownDate - now;
-    
-  // Time kalkulasi
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Kirim berdasarkan id
-  document.getElementById("countdown").innerHTML = days + "d:" + hours + ":"
-  + minutes + ":" + seconds + "";
-    
-  // Jika waktu sudah tiba maka kadaluarsa
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
